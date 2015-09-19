aa963a25d21c00cc353fd82d67fb391123a83df8
mehringer <lrm96@cornell.edu>
2015-09-19 17:17:26 -0400

    added recursive function

--- index.js
+++ index.js
@@ -1,17 +1,68 @@
+
+//-------------------------- constants ------------------------------------
+
 var app = require('express')();
 var https = require('https');
 var http = require('http');
 var server = http.Server(app);
 var io = require('socket.io')(server);
 var querystring = require('querystring');
+var languages = ["fr", "de", "ja", "ar", "ru", "en"];
 
 // API key for translate
 var yandexAPIkey = "trnsl.1.1.20150919T183321Z.d9cbecfd657d3645.6863667a9c7e61e4ebb897d5dbc0cd50503ec027";
+
+//----------------------- useful functions ------------------------------------
 
 
 app.get('/', function(request, response){
 	response.sendFile(__dirname + '/index.html');
 });
+
+function handleChatMessage(languageIndex, srclang, msg, callbackfunc) {
+	console.log("================================");
+	// terminate if hit all languages
+	if (languageIndex >= languages.length){ 
+		console.log("hit terminal condition");
+		//io.emit('chat message', msg);
+		callbackfunc(msg);
+		return msg; 
+	}
+	// get language information and key
+	var destlang = languages[languageIndex];
+	var translateoptions = {
+			key: yandexAPIkey,
+			lang: srclang + '-' + destlang,
+			text: [msg] // pass in an array of the original untranslated message
+		};
+
+	// build request URL
+	var requestURL = "https://translate.yandex.net/api/v1.5/tr.json/translate?" 
+		+ querystring.stringify(translateoptions);
+	console.log("request url: " + requestURL);
+
+	// send HTTPS Get request to Yandex
+	var translatedMsg;
+	console.log('sending request to yandex');
+	https.get(requestURL, function(response){
+		console.log('response received');
+		// response comes in a binary stream
+		response.on('data', function(data){
+			process.stdout.write(data);
+			var decoded_data = JSON.parse(data.toString('utf8')); // decode response
+			console.log("== " + decoded_data + " DECODED DATA");			
+			translatedMsg = decoded_data.text[0];
+			console.log("== " + translatedMsg + " DECODED DATA");
+
+			console.log(decoded_data.text);
+			// recursive call
+			return handleChatMessage(languageIndex+1, destlang, translatedMsg, callbackfunc);
+		});
+	});
+
+}
+
+//-------------------------- on connection ------------------------------------
 
 io.on('connection', function(socket){
 	console.log('a user connected');
@@ -19,36 +70,18 @@ io.on('connection', function(socket){
 	// receive a message
 	socket.on('chat message', function(msg){
 		console.log('message: ' + msg);
-
-		// now fuck it up! ==============================
-		var srclang = "en";
-		var destlang = "fr";
-		var translateoptions = {
-			key: yandexAPIkey,
-			lang: srclang + '-' + destlang,
-			text: [msg] // pass in an array of the original untranslated message
-		};
-
-		// build request URL
-		var requestURL = "https://translate.yandex.net/api/v1.5/tr.json/translate?" + querystring.stringify(translateoptions);
-		console.log("request url: " + requestURL);
-
-		// send HTTPS Get request to Yandex
-		console.log('sending request to yandex');
-		https.get(requestURL, function(response){
-			console.log('response received');
-
-			// response comes in a binary stream
-			response.on('data', function(data){
-				var decoded_data = JSON.parse(data.toString('utf8')); // decode response
-				console.log(decoded_data.text);
-
-				io.emit('chat message', decoded_data.text[0]); // emit translation to all users
+		handleChatMessage(0, "en", msg, 
+			function (emitMessedUpMessage) {
+				console.log('~~~~~~~~inside callback');
+				io.emit('chat message', emitMessedUpMessage);
+				return;
 			});
-		});
-
+		// send the messed up message to all users
+		//io.emit('chat message', messedUpMessage);
+		io.emit('chat message', "THIS IS A TEST MESSAGE");
+		console.log(decoded_data.text);	
 	});
-
+	console.log('READY TO DISCONNECT');
 	socket.on('disconnect', function(){
 		console.log('a user disconnected');
 	});
